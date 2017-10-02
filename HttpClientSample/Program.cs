0f0dcddcddd2689f14b884527d155de0e2d525f6
David Ebbo <david.ebbo@gmail.com>
2017-10-02 11:59:47 -0700

    Added resource level samples to HttpClientSample

--- HttpClientSample/Program.cs
+++ HttpClientSample/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -39,12 +40,73 @@ namespace HttpClientSample
             string token = await AuthenticationHelpers.AcquireTokenBySPN(tenantId, clientId, clientSecret);
 
             using (var client = new HttpClient(new LoggingHandler(new HttpClientHandler())))
+            //using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
                 client.BaseAddress = new Uri("https://management.azure.com/");
 
+                //await ListAllResourcesInSubscriptionWithPaging(client);
+                //await DeleteEmptyResourceGroups(client);
                 await TestWebAppOperations(client);
                 await TestFunctionAppOperations(client);
+            }
+        }
+
+        static async Task ListAllResourcesInSubscriptionWithPaging(HttpClient client)
+        {
+            JArray results = new JArray();
+            string url = $"/subscriptions/{Subscription}/resources?api-version=2017-08-01";
+            while (!String.IsNullOrEmpty(url))
+            {
+                using (var response = await client.GetAsync(url))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var json = await response.Content.ReadAsAsync<dynamic>();
+                    results.Merge(json.value);
+
+                    // Follow the next page of results
+                    url = json.nextLink;
+                }
+            }
+
+            Console.WriteLine(results);
+        }
+
+        static async Task DeleteEmptyResourceGroups(HttpClient client)
+        {
+            // Delete empty Resource Groups
+
+            using (var response = await client.GetAsync(
+                $"/subscriptions/{Subscription}/resourceGroups?api-version=2017-08-01"))
+            {
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsAsync<dynamic>();
+                foreach (var rg in json.value)
+                {
+                    string qqq = $"/subscriptions/{Subscription}/resourceGroups/{rg.name}/resources?api-version=2017-08-01";
+                    using (var response2 = await client.GetAsync(
+                        $"/subscriptions/{Subscription}/resourceGroups/{rg.name}/resources?api-version=2017-08-01"))
+                    {
+                        response2.EnsureSuccessStatusCode();
+
+                        json = await response2.Content.ReadAsAsync<dynamic>();
+
+                        if (json.value.Count == 0)
+                        {
+                            Console.WriteLine($"Deleting {rg.name}...");
+
+                            // Delete the Resource Group
+
+                            using (var response3 = await client.DeleteAsync(
+                                $"/subscriptions/{Subscription}/resourceGroups/{rg.name}?api-version=2017-08-01"))
+                            {
+                                response3.EnsureSuccessStatusCode();
+                                Console.WriteLine($"{rg.name} was deleted");
+                            }
+                        }
+                    }
+                }
             }
         }
 
